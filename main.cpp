2247cb0d6af66f75b4171844c287dea322ba6309
V. AwU <viceawu@outlook.com>
2025-03-16 14:16:50 +0300

    create count_neighbours

--- main.cpp
+++ main.cpp
@@ -3,14 +3,39 @@
 
 #include "lib/Cell.h"
 
+// TODO move this shit elsewere :: incapsulate into scene!
+
 int get_index(const int& i, const int& j, const int& height, const int& length) {
+    // field is considered cyclical on the edges
     int corrected_i = ((i % height) + height) % height;
     int corrected_j = ((j % length) + length) % length;
     return i * length + j;
 }
 
-void step(std::vector<Cell>& scene, const int& height, const int& length) {
+int count_neighbours(std::vector<Cell>& scene, const int& i, const int& j, const int& height, const int& length) {
+    int count = 0;
 
+    for (int di = -1; di <= 1; di++) {
+        for (int dj = -1; dj <= 1; dj++) {
+            if (di == 0 && dj == 0) continue;
+
+            count += scene[get_index(i + di, j + dj, height, length)];
+        }
+    }
+
+    return count;
+}
+
+void step(std::vector<Cell>& scene, const int& height, const int& length) {
+    // std::vector<int> scene_copy(scene);
+
+    // Iterate over all of the cells, count neighbours and update accordingly
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < length; ++j) {
+            std::cout << scene[i * length + j];
+        }
+        std::cout << "\n";
+    }
 }
 
 int main(int, char**){
@@ -32,6 +57,8 @@ int main(int, char**){
         }
         std::cout << "\n";
     }
+
+    std::cout << count_neighbours(scene, 1, 2, height, length) << "\n";
 
     return 0;
 }
