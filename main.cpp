c6d99336cebd6dff32419b4462dfead3eca655de
V. AwU <viceawu@outlook.com>
2025-03-25 15:47:12 +0300

    experimenting with glfw - icon loading

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <iostream>
+#define STB_IMAGE_IMPLEMENTATION
+#include "ext/stb/include/stb_image.h"
 
 static void glfwError(int id, const char* description) {
     std::cout << description << std::endl;
@@ -15,13 +17,23 @@ int main(int, char**){
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "I FUCKING MADE A WINDOW", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Game of Life", NULL, NULL);
+
     if (!window) {
         glfwTerminate();
-        return 1;
+        return 2;
     }
 
-    while (true) {}
+    GLFWimage images[1]; 
+    images[0].pixels = stbi_load("logo.png", &images[0].width, &images[0].height, 0, 4); //rgba channels
+    if (images[0].pixels != NULL) glfwSetWindowIcon(window, 1, images);
+
+    while (!glfwWindowShouldClose(window)) {
+        // render(window);
+    
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     glfwTerminate();
     return 0;
